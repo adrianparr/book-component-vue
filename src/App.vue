da25fbc1d6aba5336e4c43d07cf7b7776fa84cc0
Adrian Parr <adrianparr@gmail.com>
2019-01-21 21:49:29 +0000

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <select class="select" v-model="selected">
+      <option v-for="book in books" :value="book.id" :key="book.id">
+        {{ book.title }}
+      </option>
+    </select>
+    <Book :id="books[selected].id" :title="books[selected].title" :author="books[selected].author" :age-group="books[selected].ageGroup" :cover-image="books[selected].coverImage" :cover-image-path="coverImagePath"></Book>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Book from "./components/Book.vue";
+
+const dataUrl = 'book-data.json';
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      books: [{}],
+      selected: 0,
+      coverImagePath: './img/'
+    };
+  },
   components: {
-    HelloWorld
+    Book
+  },
+  mounted() {
+    this.loadData();
+  },
+  methods: {
+    loadData: function() {
+      var vue = this;
+      fetch(dataUrl)
+        .then(response => {
+          return response.json().then(data => {
+            if (response.ok) {
+              return data;
+            } else {
+              return Promise.reject({ status: response.status, data });
+            }
+          });
+        })
+        .then(data => {
+          vue.books = data.books;
+        })
+        .catch(error => {
+          /* eslint-disable no-alert */
+          alert("Oops! An error occurred.");
+          /* eslint-disable no-console */
+          console.log(error);
+        });
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #4b5055;
+}
+.select {
+  border: 1px solid #d5d6d6;
+  border-radius: 20px;
+  color: #4b5055;
+  display: block;
+  font-size: 1.1rem;
+  height: 40px;
+  margin: 2rem auto;
 }
 </style>
